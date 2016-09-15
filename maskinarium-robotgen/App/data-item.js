65dde323e40748ef248c261adac4affa13083b34
davidsyntex <syntex@outlook.com>
2016-09-15 20:17:08 +0200

    Close #10

---
+++ maskinarium-robotgen/App/data-item.js
@@ -0,0 +1,33 @@
+﻿window.itemsData = {
+    "artifacts": [
+        {
+            "name": "Chockbatong",
+            "book":"Maskinarium",
+            "description": "Ett högteknologiskt närstridsvapen byggt för att oskadliggöra målet utan att döda.",
+            "effect": "Prylbonus +2 på bekämpa, vapenskada 1. Vid träff tar levande varelser T6 poäng stress utöver vanlig skada. Rustning skyddar inte mot chockbatongens effekter, men det gör däremot monsters pansar. Energivapen. Elektroniskt föremål.",
+            "unKrav": "Teknologi 60",
+            "unBonus": "Teknologi +T6"
+        },{
+            "name": "CHOCKPISTOL",
+            "book":"Maskinarium",
+            "description": "En liten och diskret tingest som går att dölja i handen. Avfyrar en stark ljusblixt som fräser högt, avsedd att tillfälligt slå ut människor och levande varelser utan att skada dem.",
+            "effect": "Lätt vapen med prylbonus +2 på beskjuta och vapenskada 2. Vid träff tar levande varelser stress istället för skada. Naturligt pansar ger skydd men rustningar fungerar inte. Energivapen. Elektroniskt föremål.",
+            "unKrav": "Teknologi 60",
+            "unBonus": "Teknologi +T6"
+        },{
+            "name": "DRÖNARVERKTYG",
+            "book":"Maskinarium",
+            "description": "En liten drönare som går att hålla i en griphand. Chassit är repigt och den ström av uppmuntrande förslag den lille gynnaren häver ur sig tar aldrig slut. Drönare fäller på kommando ut det verktyg användaren behöver.",
+            "effect": "Drönaren är snacksalig men står man ut med pladdret ger den bra tips. Ger prylbonus +3 på reparera, tillverka och mecka. Elektroniskt föremål. Kan reparera även elektroniska föremål.",
+            "unKrav": "Teknologi 70",
+            "unBonus": "Teknologi +T6"
+        },{
+            "name": "ELEKTRONIKVERKTYG",
+            "book":"Maskinarium",
+            "description": "En liten verktygssats med högteknologiska verktyg för reparation av elektroniska apparater, som laserpistoler och robotar.",
+            "effect": "Prylbonus +2 vid reparation av elektroniska föremål och maskiner. Utan dessa verktyg får icke-robotar -3 på alla försök att reparera elektroniska apparater.",
+            "unKrav": "Teknologi 50",
+            "unBonus": "Teknologi +T6"
+        },
+    ]
+};
